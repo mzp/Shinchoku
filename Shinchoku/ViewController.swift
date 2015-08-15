e84069c7f76dc95ebb56fc493a97fcd6ca20a82b
mzp <mzpppp@gmail.com>
2015-08-15 13:17:02 +0900

    Initial Commit

---
+++ Shinchoku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shinchoku
+//
+//  Created by mzp on 8/15/15.
+//  Copyright © 2015 mzp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
